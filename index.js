6e20aefb8575971ae967b0bb8b177103bdfad96c
Timo <30553356+y21@users.noreply.github.com>
2018-01-17 17:55:02 +0100

    ChannelInfo Command

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ client.on("message", (message) => {
   if(message.content.toLowerCase().startsWith(config.prefix + "help")){
     helpModule(client, message, Discord, config.prefix);
   }
+  else if(message.content.toLowerCase() === prefix + "channelinfo"){
+          require("./src/ChannelInfo.js")(Discord, message);
+  }
   else if(message.content.toLowerCase() === config.prefix + "ping"){
 	  message.channel.send("Pinging... ").then(m => m.edit("Heartbeat: `" + (new Date() - m.createdAt) + "ms`\nAverage Ping: `" + client.ping + "ms`"));
   }
