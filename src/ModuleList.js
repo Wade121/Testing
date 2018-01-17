a022ce47e4a2622b52998d4d14d7db80af2f40d3
Timo <30553356+y21@users.noreply.github.com>
2018-01-17 17:55:26 +0100

    More modules

--- src/ModuleList.js
+++ src/ModuleList.js
@@ -1,9 +1,13 @@
 module.exports = function(prefix){
   return [
-    prefix + "help [-]",
-    "ping [-]",
-    "eval [Evaluation]",
-	"invite [-]",
-	"about [-]",
+    prefix + "help [-] - Help page",
+    "ping [-] - Ping",
+    "eval [Evaluation] - Evaluate JS Code (Owner-only)",
+	"invite [-] - Get invite link",
+	"about [-] - About me",
+    "r / " + prefix + "reverse [string] - Reverse a text",
+    "game [string] - Set game of scale (Owner-only)",
+    "discriminator [discrim] - Filter users by their discriminator",
+    "channelinfo - Information about this channel"
   ];
 }
