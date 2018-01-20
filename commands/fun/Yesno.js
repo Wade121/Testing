d50952a2c0d040b441e06650c5d3aba09a38cf84
Timo <30553356+y21@users.noreply.github.com>
2018-01-20 17:53:55 +0100

    Yesno command

---
+++ commands/fun/Yesno.js
@@ -0,0 +1,27 @@
+const bot = require("../../Bot.js");
+const Bot = new bot();
+const Discord = require("discord.js");
+
+module.exports = class extends bot {
+    /**
+     * @param  {object} message Message object
+     */
+    constructor(message){
+        super();
+        this.message = message;
+        this._run = () => {
+            this.message.channel.send(Math.random() > Math.random() ? "Yes" : "No").catch(e => console.log(e));
+        };
+    }
+    /**
+     * @param  {function} fn Function
+     */
+    set runFN(fn){
+        this._run = fn;
+    }
+    run() {
+        if(this.commands.fun.ping.status){
+            this._run(this.message);
+        }
+    }
+}
