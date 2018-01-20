89cc7f89571a85339ca42f6c3acf0afaa154e128
Timo <30553356+y21@users.noreply.github.com>
2018-01-20 18:38:57 +0100

    Bug fixes

--- commands/CommandHandler.js
+++ commands/CommandHandler.js
@@ -8,6 +8,7 @@ module.exports = class extends bot {
      * @returns {boolean} 
      */
     static checkCommand(command) {
+        super();
         if(Object.keys(Bot.commands.fun).includes(command) || 
         Object.keys(Bot.commands.owner).includes(command) || 
         Object.keys(Bot.commands.moderation).includes(command) ||
@@ -19,10 +20,27 @@ module.exports = class extends bot {
      * @param  {object} message Message object
      */
     static runCommand(command, message) {
+        let __command = command;
         command += ".js";
         command = command.toLowerCase();
         command = command.charAt(0).toUpperCase() + command.substr(1).toLowerCase();
-        let _command = require(`./fun/${command}`) || require(`./miscellaneous/${command}`) || require(`./moderation/${command}`) || require(`./owner/${command}`);
+        let _command;
+        if(fs.existsSync(`./commands/fun/${command}`)){
+            _command = require(`./fun/${command}`);
+        }else if(fs.existsSync(`./commands/miscellaneous/${command}`)){
+            _command = require(`./miscellaneous/${command}`);
+        }else if(fs.existsSync(`./commands/moderation/${command}`)){
+            _command = require(`./moderation/${command}`);
+        }else if(fs.existsSync(`./commands/owner/${command}`)){
+            _command = require(`./owner/${command}`);
+        }
+        if(Object.keys(Bot.commands.owner).includes(__command)){
+            if(message.author.id === this.owner){
+                new _command(message).run();
+            }else{
+                message.react("389090190506852353");
+            }
+        }
         new _command(message).run();
     }
 }
