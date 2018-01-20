fed6ef644b07a5eee1006751de71f5350839d86d
Timo <30553356+y21@users.noreply.github.com>
2018-01-20 21:37:59 +0100

    Typo
    Forgot 'this'

--- commands/moderation/Mute.js
+++ commands/moderation/Mute.js
@@ -14,7 +14,7 @@ module.exports = class extends bot {
                 if(this.message.mentions.members.first().roles.has(this.message.guild.roles.find("name", "muted"))) return message.reply("mentioned user is already muted.");
                 if(this.message.guild.roles.find("name", "muted")){
                     this.message.mentions.members.first().addRole(this.message.guild.roles.find("name", "muted"));
-                    this.message.reply("successfully muted " + message.mentions.users.first().tag)
+                    this.message.reply("successfully muted " + this.message.mentions.users.first().tag)
                 }else{
                     this.message.guild.createRole({name: "muted"}).then(r => this.message.reply("created a role called 'muted' because there was no and added it to " + this.message.mentions.users.first().tag));
                     this.message.mentions.members.first().addRole(this.message.guild.roles.find("name", "muted"));
