e014d98923255be36872cb2cef4d72e365fb3ccf
Timo <30553356+y21@users.noreply.github.com>
2018-01-20 17:32:24 +0100

    Ping command + jsdoc

---
+++ commands/fun/Ping.js
@@ -0,0 +1,27 @@
+const bot = require("../../Bot.js");
+const Bot = new bot();
+const Discord = require("discord.js");
+
+module.exports = class extends bot {
+    /**
+     * @param  {object} message Message object
+     */
+    constructor(message){
+        super();
+        this.message = message;
+        this._run = () => {
+            this.message.channel.send("Pinging... ").then(m => m.edit("Heartbeat: `" + (new Date() - m.createdAt) + "ms`\nAverage Ping: `" + this.message.client.ping + "ms`"));
+        }
+    }
+    /**
+     * @param  {function} fn Function
+     */
+    set runFN(fn){
+        this._run = fn;
+    }
+    run() {
+        if(this.commands.fun.ping.status){
+            this._run(this.message);
+        }
+    }
+}
