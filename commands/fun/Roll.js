a6d1899994569de5aec9c218d6290cc4b4c2d2f4
Timo <30553356+y21@users.noreply.github.com>
2018-01-27 20:19:46 +0100

    Roll command

---
+++ commands/fun/Roll.js
@@ -0,0 +1,27 @@
+const bot = require("../../Bot.js");
+const Bot = new bot();
+const Discord = require("discord.js");
+
+module.exports = class extends bot {
+    /**
+     * @param  {object} message Message object
+     */
+    constructor(message){
+        super();
+        this.message = message;
+        this._run = () => {
+            this.message.channel.send(Math.floor(Math.random() * 100) + 1).catch(e => this.message.reply("An error occured: `" + e + "`"));
+        };
+    }
+    /**
+     * @param  {function} fn Function
+     */
+    set runFN(fn){
+        this._run = fn;
+    }
+    run() {
+        if(new bot().commands.fun.discriminator.status){
+            this._run(this.message);
+        }
+    }
+}
