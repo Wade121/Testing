621673a873c88ac0daae8ba2f42fc3af912063ac
Timo <30553356+y21@users.noreply.github.com>
2018-01-27 22:01:55 +0100

    Report command added
    Report bugs using :report <Bug>

---
+++ commands/miscellaneous/Report.js
@@ -0,0 +1,38 @@
+const bot = require("../../Bot.js");
+const Bot = new bot();
+const Discord = require("discord.js");
+
+module.exports = class extends bot {
+    /**
+     * @param  {object} message Message object
+     */
+    constructor(message){
+        super();
+        this.message = message;
+        this._run = () => {
+            try {
+                if(this.message.content.split(" ").length < 1) return this.message.reply("i can't read your mind, please tell me your problem.");
+                this.message.client.users.get("312715611413413889").send(new Discord.RichEmbed()
+                    .setColor("RANDOM")
+                    .addField(`Reporter: **${this.message.author.tag}**`, "\u200b")
+                    .addField(`Guild: **${!this.message.guild ? "/" : this.message.guild.name}**`, `Guild members: **${!this.message.guild ? "/" : this.message.guild.memberCount}**`)
+                    .addField(`Message`, "```https\n" + this.message.content.split(" ").slice(1).join(" ") + "\n```")
+                ).catch(e => this.message.reply("An error occured: `" + e + "`"));
+                this.message.reply("Bug has been submitted!");
+            } catch (e) {
+                this.message.reply("An error occured: `" + e + "`");
+            }
+        };
+    }
+    /**
+     * @param  {function} fn Function
+     */
+    set runFN(fn){
+        this._run = fn;
+    }
+    run() {
+        if(new bot().commands.miscellaneous.report.status){
+            this._run(this.message);
+        }
+    }
+};
