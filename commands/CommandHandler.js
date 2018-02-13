717c8c62df4cda4fb27f53373c9acdc023918b80
Timo <30553356+y21@users.noreply.github.com>
2018-02-13 19:39:20 +0100

    commandCounter + tags category

--- commands/CommandHandler.js
+++ commands/CommandHandler.js
@@ -15,7 +15,8 @@ module.exports = class extends bot {
         Object.keys(Bot.commands.owner).includes(command) || 
         Object.keys(Bot.commands.moderation).includes(command) ||
         Object.keys(Bot.commands.miscellaneous).includes(command) ||
-        Object.keys(Bot.commands["image-manipulation"]).includes(command)
+            Object.keys(Bot.commands["image-manipulation"]).includes(command)||
+            Object.keys(Bot.commands.tags).includes(command)
         ) return true;
         else return false;
     }
@@ -39,6 +40,8 @@ module.exports = class extends bot {
             _command = require(`./owner/${command}`);
         }else if(fs.existsSync(`./commands/image-manipulation/${command}`)){
             _command = require(`./image-manipulation/${command}`);
+        }else if(fs.existsSync(`./commands/tags/${command}`)) {
+            _command = require(`./tags/${command}`);
         }
         if(Object.keys(Bot.commands.owner).includes(__command)){
             if(message.author.id != Bot.owner) return message.react("389090190506852353");
@@ -50,6 +53,21 @@ module.exports = class extends bot {
         if(__command == "ban" || __command == "softban" || __command == "unban"){
             if(!message.member.hasPermission('BAN_MEMBERS')) return message.react("389090190506852353");
         }
+
+
         new _command(message).run();
+    }
+  
+  static async commandCounter(db, message){
+      try {
+        let commandRuns = await db.get('SELECT * FROM commands WHERE command="' + message.content.split(" ")[0].substr(1).toLowerCase() + '"')
+        if(!commandRuns) db.run('INSERT INTO commands VALUES ("' + message.content.split(" ")[0].substr(1).toLowerCase() + '", 1)');
+        else {
+            commandRuns = parseInt(commandRuns.uses);
+            db.run('UPDATE commands SET uses=' + (++commandRuns) + ' WHERE command="' + message.content.split(" ")[0].substr(1).toLowerCase() + '"');
+        }
+        }catch(e){
+     console.log(e); 
+  }
     }
 }
