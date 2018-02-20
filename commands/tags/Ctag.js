31e9a20ce04a4bf486301bf87cf918805b0b2004
Timo <30553356+y21@users.noreply.github.com>
2018-02-20 20:23:33 +0100

    Invalid expression for tag creation

--- commands/tags/Ctag.js
+++ commands/tags/Ctag.js
@@ -15,6 +15,13 @@ module.exports = class extends bot {
         this.message = message;
         this._run = async () => {
             try {
+                const tag = {
+                    name: this.message.content.split(" ")[1],
+                    content: this.message.content.split(" ").slice(2).join(" "),
+                    args: this.message.content.split(" ").slice(1).join(" "),
+                    expression: /^\w{1,14}\s(\w|\s){3,1000}$/g
+                };
+                if(!tag.expression.test(tag.args)) return this.message.reply("tag is too long/short.```ini\nRegular Expression: " + tag.expression + "\n```");
                 let _q = await db.get('select * from tags where name="' + this.message.content.split(" ")[1] + '"');
                 if(_q) return this.message.reply("tag with that name already exists!");
                 db.run('insert into tags values ("' + this.message.author.id + '", "' + this.message.content.split(" ").slice(2).join(" ") + '", "' + Date.now() + '", 0, "' + this.message.content.split(" ")[1] + '")').then(r => {
