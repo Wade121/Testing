3dc9254e8bc5ee24cc63a1dbf69664be73f7cbfa
Timo <30553356+y21@users.noreply.github.com>
2018-04-21 17:04:08 +0200

    Refracted a lot

--- commands/CommandHandler.js
+++ commands/CommandHandler.js
@@ -2,7 +2,7 @@ const bot = require("../Bot.js");
 const Bot = new bot();
 const fs = require("fs");
 const cdown = require("../Cooldown.js");
-const Cooldown = new cdown(1000);
+const Cooldown = new cdown(3000);
 
 module.exports = class extends bot {
     /**
@@ -13,66 +13,34 @@ module.exports = class extends bot {
         super();
     }
     static checkCommand(command) {
-        if(Object.keys(Bot.commands.fun).includes(command) || 
-        Object.keys(Bot.commands.owner).includes(command) || 
-        Object.keys(Bot.commands.moderation).includes(command) ||
-        Object.keys(Bot.commands.miscellaneous).includes(command) ||
-            Object.keys(Bot.commands["image-manipulation"]).includes(command)||
-            Object.keys(Bot.commands.tags).includes(command)
-        ) return true;
-        else return false;
+        return Object.values(this.commands).map(e=>Object.keys(e)).filter(o=>o.includes('sqlget')).length>0;
     }
     /**
      * @param  {string} command Command (one of <bot>.commands)
      * @param  {object} message Message object
      */
     static runCommand(command, message) {
-        let __command = command;
         if(Cooldown.check(message.author.id)) return message.reply("Slow down! Wait another `" + ((Cooldown.delay - Cooldown.getTimeDiff(message.author.id)) / 1000) + "` seconds.");
         if(Cooldown.cooldowns.has(message.author.id)) Cooldown.remove(message.author.id);
         Cooldown.add(message.author.id, Date.now());
-        command += ".js";
-        command = command.toLowerCase();
-        command = command.charAt(0).toUpperCase() + command.substr(1).toLowerCase();
-        let _command;
-        if(fs.existsSync(`./commands/fun/${command}`)){
-            _command = require(`./fun/${command}`);
-        }else if(fs.existsSync(`./commands/miscellaneous/${command}`)){
-            _command = require(`./miscellaneous/${command}`);
-        }else if(fs.existsSync(`./commands/moderation/${command}`)){
-            _command = require(`./moderation/${command}`);
-        }else if(fs.existsSync(`./commands/owner/${command}`)){
-            _command = require(`./owner/${command}`);
-        }else if(fs.existsSync(`./commands/image-manipulation/${command}`)){
-            _command = require(`./image-manipulation/${command}`);
-        }else if(fs.existsSync(`./commands/tags/${command}`)) {
-            _command = require(`./tags/${command}`);
-        }
-        if(Object.keys(Bot.commands.owner).includes(__command)){
-            if(message.author.id != Bot.owner) return message.react("389090190506852353");
-        }
-        // Hardcode
-        if(__command == "clear" || __command == "mute" || __command == "unmute"){
-            if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.react("389090190506852353");
-        }
-        if(__command == "ban" || __command == "softban" || __command == "unban"){
-            if(!message.member.hasPermission('BAN_MEMBERS')) return message.react("389090190506852353");
-        }
-
-
-        new _command(message).run();
+        let commandInfo = {
+            category: Object.entries(this.commands).filter(e=>Object.keys(e[1]).includes(message.content.split(" ")[0].substr(Bot.prefix.length)))[0][0],
+            command: message.content.split(" ")[0].substr(this.prefix.length)
+        };
+        let module = new (require(`./${commandInfo.category}/${commandInfo.command}.js`))(message);
+        module.run();
     }
 
-  static async commandCounter(db, message){
-      try {
-        let commandRuns = await db.get('SELECT * FROM commands WHERE command="' + message.content.split(" ")[0].substr(2).toLowerCase() + '"')
-        if(!commandRuns) db.run('INSERT INTO commands VALUES ("' + message.content.split(" ")[0].substr(2).toLowerCase() + '", 1)');
-        else {
-            commandRuns = parseInt(commandRuns.uses);
-            db.run('UPDATE commands SET uses=' + (++commandRuns) + ' WHERE command="' + message.content.split(" ")[0].substr(2).toLowerCase() + '"');
-        }
+    static async commandCounter(db, message){
+        try {
+            let commandRuns = await db.get('SELECT * FROM commands WHERE command="' + message.content.split(" ")[0].substr(2).toLowerCase() + '"')
+            if(!commandRuns) db.run('INSERT INTO commands VALUES ("' + message.content.split(" ")[0].substr(2).toLowerCase() + '", 1)');
+            else {
+                commandRuns = parseInt(commandRuns.uses);
+                db.run('UPDATE commands SET uses=' + (++commandRuns) + ' WHERE command="' + message.content.split(" ")[0].substr(2).toLowerCase() + '"');
+            }
         }catch(e){
-             console.log(e);
+            console.log(e);
         }
     }
 }
