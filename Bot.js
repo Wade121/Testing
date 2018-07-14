145c390c3010e0624a7e4af3d91b72eea4c6edbd
Wade121 <Wadesmith201@hotmail.com>
2018-07-14 12:34:19 +1000

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,9 +1,9 @@
 module.exports = class {
     constructor() {
         this.version = "1.7";
-        this.token = "";
+        this.token = "NDY3NTA3NjA0OTQwMjU5MzMw.Dirsbg.GL_TZHKfsSW6NegFzUSeYrGuTuE";
         this.prefix = "s.";
-        this.owner = "312715611413413889";
+        this.owner = " 292871335305019394";
         this.commands = require("./commands/CommandList");
     }
 }
